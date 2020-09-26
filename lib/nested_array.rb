1088e6cc739df5417dc34ca7cfad42244187b736
akam alam <arundelfootball23@gmail.com>
2020-09-26 21:45:49 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,12 +1,30 @@
 # Examples inspired by U. S. National Organic Standards
 
-ORGANIC_PRODUCE = [
+# ORGANIC_PRODUCE = [
+#   "Strawberries",
+#   "Potatoes",
+#   "Grapes",
+#   "Avocadoes",
+#   "Asparagus"
+# ]
+
+# CONVENTIONAL_PRODUCE = [
+#   "Grapefruit",
+#   "Pineapple",
+#   "Oranges",
+#   "Watermelon",
+#   "Eggplant"
+# ]
+
+def assembled_matrix
+  produce_storage_room = [
+  ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
   "Grapes",
   "Avocadoes",
   "Asparagus"
-]
+],
 
 CONVENTIONAL_PRODUCE = [
   "Grapefruit",
@@ -15,27 +33,51 @@ CONVENTIONAL_PRODUCE = [
   "Watermelon",
   "Eggplant"
 ]
-
-def assembled_matrix
+]
+ 
+ 
+  
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 end
 
-def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
-  # uses the arrays of conventional and organic produce as before.
-  # However, this time, sort each internal array alphabetically by the first character
-end
+# def sorted_matrix
 
-def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
-end
+# produce_storage_room =[
+  
+#   conventional_produce = [
+#   "Grapefruit",
+#   "Pineapple",
+#   "Oranges",
+#   "Watermelon",
+#   "Eggplant" ],
+   
+#   organic_produce = [
+#   "Strawberries",
+#   "Potatoes",
+#   "Grapes",
+#   "Avocadoes",
+#   "Asparagus"     ]
+# ]
+# produce_storage_room.sort 
 
-def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
-end
+#   # Using Array literal syntax only, build another nested array that 
+#   # uses the arrays of conventional and organic produce as before.
+#   # However, this time, sort each internal array alphabetically by the first character
+# end
+
+# def matrix_lookup(matrix, row, column)
+  
+  
+  
+#   # Given any matrix (array of arrays), a row index and a column index, 
+#   # Return the matrix's content at that row and and column
+# end
+
+# def matrix_update(matrix, row, column, new_value)
+#   # Given any matrix (array of arrays), a row index and a column index, 
+#   # Update the matrix location at that row and column to have the value of new_value
+#   # Return the updated matrix
+# end
